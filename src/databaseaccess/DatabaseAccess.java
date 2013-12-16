cf2bfa4fe1a5c67bbe9ea358fb2d58bcd0562892
Jesse-Voight <24051java1@gmail.com>
2013-12-16 14:16:27 +1300

    Added Basic GUI interface

--- src/databaseaccess/DatabaseAccess.java
+++ src/databaseaccess/DatabaseAccess.java
@@ -5,6 +5,7 @@
 package databaseaccess;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,31 +16,39 @@ public class DatabaseAccess {
     /**
      * @param args the command line arguments
      */
+    String lastResult = "";
+    
     public static void main(String[] args) {
-        executeSQL("hi");
+        //executeSQL("hi");
         
     }
-    public static String executeSQL(String result){
-        String url = "jdbc:mysql://localhost:3306/";
+    public static ArrayList executeSQL(String inputQuery){
+        String url = "jdbc:mysql://websrv:3306/";
         String dbName = "assetDB";
         String driver = "com.mysql.jdbc.Driver";
-        String userName = "root";
-        String password = "root";
+        String userName = "jessvoig";
+        String password = "qzpm9876";
+        String unk = "";
+        ArrayList resultList = new ArrayList();
+
         try {
             Class.forName(driver).newInstance();
             try (Connection conn = DriverManager.getConnection(url + dbName, userName, password)) {
                 Statement st = conn.createStatement();
-                ResultSet res = st.executeQuery("Select * FROM PC");
+                //ResultSet res = st.executeQuery("inputQuery");
+                ResultSet res = st.executeQuery("Select * FROM PC WHERE Name like 'WSC%' or Name like 'LTC%' order by Name");
                 while (res.next()){
-                    int name = res.getInt("Name");
-                    String id = res.getString("ID");
-                    System.out.println(id + "\t" + name);
+                    String name = res.getString("Name");
+                    String id = res.getString("idPC");
+                    System.out.println(id + "\t " + name);
+                    resultList.add(id + " " + name + "\n");
                 }
+                conn.close();
             }
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return resultList;
     }
     // TODO code application logic here
 }
